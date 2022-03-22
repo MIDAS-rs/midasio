6d83f5ad54a640b8225560b93f3dafab4dda8043
Daniel Duque <daniel.duque@cern.ch>
2022-03-22 01:23:18 -0700

    Add data types

--- src/lib.rs
+++ src/lib.rs
@@ -84,8 +84,16 @@ pub enum DataType {
     F32,
     /// 64-bits floating-point number.
     F64,
+    /// 32-bits bitfield.
+    Bit32,
+    /// Zero-terminated string.
+    Str,
     /// User-defined structure with fixed size in bytes.
     Struct,
+    /// Signed 64-bits integer.
+    I64,
+    /// Unsigned 64-bits integer.
+    U64,
 }
 
 impl DataType {
@@ -102,6 +110,7 @@ impl DataType {
     /// ```
     pub fn size(&self) -> Option<usize> {
         match *self {
+            // If you add a new type here, remember to add it as well in TryFrom unsigned below
             DataType::Byte => Some(size_of::<u8>()),
             DataType::I8 => Some(size_of::<i8>()),
             DataType::U8 => Some(size_of::<u8>()),
@@ -109,10 +118,14 @@ impl DataType {
             DataType::I16 => Some(size_of::<i16>()),
             DataType::U32 => Some(size_of::<u32>()),
             DataType::I32 => Some(size_of::<i32>()),
-            DataType::Bool => Some(size_of::<bool>()),
+            DataType::Bool => Some(4), // 4 bytes bool instead of normal 1 byte
             DataType::F32 => Some(size_of::<f32>()),
             DataType::F64 => Some(size_of::<f64>()),
+            DataType::Bit32 => Some(4), // 4 bytes = 32 bits
+            DataType::Str => None,
             DataType::Struct => None,
+            DataType::I64 => Some(size_of::<i64>()),
+            DataType::U64 => Some(size_of::<u64>()),
         }
     }
 }
@@ -145,7 +158,14 @@ macro_rules! impl_try_type_from {
                     8 => Ok(DataType::Bool),
                     9 => Ok(DataType::F32),
                     10 => Ok(DataType::F64),
+                    11 => Ok(DataType::Bit32),
+                    12 => Ok(DataType::Str),
+                    // 13 missing. Array. What does that mean?
                     14 => Ok(DataType::Struct),
+                    // 15 missing. Key. What does that mean?
+                    // 16 missing. Link. What does that mean?
+                    17 => Ok(DataType::I64),
+                    18 => Ok(DataType::U64),
                     _ => Err(TryDataTypeFromUnsignedError),
                 }
             }
